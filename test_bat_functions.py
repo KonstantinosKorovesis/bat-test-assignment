f4cc9229e4e1d5588ff289bc84fca6cb63b2d287
= <t8210063@aueb.gr>
2025-04-17 17:40:31 +0300

    Test get_bat_vehicle using a fixture

--- test_bat_functions.py
+++ test_bat_functions.py
@@ -17,6 +17,7 @@ def test_calculate_bat_power():
     assert calculate_bat_power(1000) == 42000
     assert calculate_bat_power(10000) == 420000
 
+
 # Parametrization Tests
 @pytest.mark.parametrize("distance, expected_signal_strength", [
     (0, 100), (1, 90), (2, 80), (3, 70), (4, 60), (5, 50), (10, 0),
@@ -24,3 +25,24 @@ def test_calculate_bat_power():
 ])
 def test_signal_strength(distance, expected_signal_strength):
     assert signal_strength(distance) == expected_signal_strength
+
+
+# Fixture Tests
+@pytest.fixture(scope="function")
+def bat_vehicles():
+    return {
+        'Batmobile': {'speed': 200, 'armor': 80},
+        'Batwing': {'speed': 300, 'armor': 60},
+        'Batcycle': {'speed': 150, 'armor': 50},
+    }
+
+def test_get_bat_vehicle(bat_vehicles):
+    for vehicle_name in bat_vehicles.keys():
+        vehicle_info = get_bat_vehicle(vehicle_name)
+        assert vehicle_info == bat_vehicles[vehicle_name]
+        assert vehicle_info["speed"] == bat_vehicles[vehicle_name]["speed"]
+        assert vehicle_info["armor"] == bat_vehicles[vehicle_name]["armor"]
+
+    for unknown_vehicle_name in ["Batboat", "Batcopter", "Batplane"]:
+        with pytest.raises(ValueError, match=f"Unknown vehicle: {unknown_vehicle_name}"):
+            get_bat_vehicle(unknown_vehicle_name)
